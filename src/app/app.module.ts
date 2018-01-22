b3af7f5c1aeed43b2c2f3046a825d6e5605875ef
Nikolay Gelemerov <nikolay.gelemerov@imperiaonline.org>
2018-01-22 11:51:52 +0200

    set up server side rendering configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'server-side-rendering'}),
     AppRoutingModule
   ],
   providers: [],
